f8b249a3a5be57a8617e0b049fabccb548907eed
Anil_Ramcharran <anil.gitme@gmail.com>
2021-02-21 10:18:00 -0500

    finished the PrivateRoute component

--- src/helpers/axiosWithAuth.js
+++ src/helpers/axiosWithAuth.js
@@ -3,11 +3,14 @@ import axios from "axios";
 //Task List:
 //Build and export a function used to send in our authorization token
 
-export const axiosWithAuth = () => {
+const axiosWithAuth = () => {
     const token = localStorage.getItem('token');
+    console.log('axiosWithAuth', token);
     return axios.create({
         headers: {
             Authorization: token
         }
     })
 }
+
+export default axiosWithAuth;
